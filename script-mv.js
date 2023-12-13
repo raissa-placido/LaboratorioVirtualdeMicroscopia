80a29d8d447ca69036cb45827bcc57ff8fdb0454
raisspl <87621789+raisspl@users.noreply.github.com>
2023-12-12 22:01:31 -0300

    alterações 12/12/2023

--- script-mv.js
+++ script-mv.js
@@ -1,36 +1,80 @@
 
-    var coll = document.getElementsByClassName("collapsible");
-    var i;
 
-    for (i = 0; i < coll.length; i++) {
-      coll[i].addEventListener("click", function () {
-        this.classList.toggle("active2");
-        var content = this.nextElementSibling;
-        if (content.style.maxHeight) {
-          content.style.maxHeight = null;
-        } else {
-          content.style.maxHeight = content.scrollHeight + "px";
-        }
-      });
+//leia mais
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function () {
+    this.classList.toggle("active2");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
     }
+  });
+}
 
-    /*menu*/
-    $(document).ready(function(){
 
-      $('.sub-btn').click(function(){
-        $(this).next('.sub-menu').slideToggle();
-        $(this).find('.dropdown').toggleClass('rotate');
-      });
 
-      $('.menu-btn').click(function(){
-        $('.side-bar').addClass('active');
-        $('.menu-btn').css("visibility", "hidden");
-      });
 
-      $('.close-btn').click(function(){
-        $('.side-bar').removeClass('active');
-        $('.menu-btn').css("visibility", "visible");
-      });
+//Aumentar e diminuir fonte
 
-    });
-    /*fim menu*/
+var $btnAumentar = $("#btnAumentar");
+var $btnDiminuir = $("#btnDiminuir");
+var $elemento = $("body").find("*"); //encontra todos os elementos dentro do #content
+var fonts = [];
+
+function obterTamanhoFonte() {
+  for (var i = 0; i < $elemento.length; i++) {
+    fonts.push(parseFloat($elemento.eq(i).css('font-size')));
+  }
+}
+obterTamanhoFonte();
+$btnAumentar.on('click', function() {
+  for (var i = 0; i < $elemento.length; i++) {
+    ++fonts[i];
+    $elemento.eq(i).css('font-size', fonts[i]);
+  }
+});
+
+$btnDiminuir.on('click', function() {
+  for (var i = 0; i < $elemento.length; i++) {
+    --fonts[i];
+    $elemento.eq(i).css('font-size', fonts[i]);
+  }
+});
+//Modo noturno
+const changeThemeBtn = document.querySelector("#change-theme");
+
+// Toggle dark mode
+function toggleDarkMode() {
+  document.body.classList.toggle("dark");
+}
+
+// Load light or dark mode
+function loadTheme() {
+  const darkMode = localStorage.getItem("dark");
+
+  if (darkMode) {
+    toggleDarkMode();
+  }
+}
+
+loadTheme();
+
+changeThemeBtn.addEventListener("change", function () {
+  toggleDarkMode();
+
+  // Save or remove dark mode from localStorage
+  localStorage.removeItem("dark");
+
+  if (document.body.classList.contains("dark")) {
+    localStorage.setItem("dark", 1);
+  }
+});
+
+
+
